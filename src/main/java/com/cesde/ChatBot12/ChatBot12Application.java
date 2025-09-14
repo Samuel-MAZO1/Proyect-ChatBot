7e2f16c2ab0d17df9bceab86ebd2a881ed6cab64
Samuel Mazo <smazo870@gmail.com>
2025-09-13 19:43:26 -0500

    Inicio del proyecto ChatBot

---
+++ src/main/java/com/cesde/ChatBot12/ChatBot12Application.java
@@ -0,0 +1,13 @@
+package com.cesde.ChatBot12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBot12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBot12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesde/ChatBot12/ChatBot12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesde.ChatBot12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBot12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
